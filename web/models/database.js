57c28d31d790a551667e7c6fb4f92e7b2f3f5b19
kamilsz <k.szalek@inwebit.pl>
2017-08-31 20:41:49 +0200

    Changes directories

---
+++ web/models/database.js
@@ -0,0 +1,8 @@
+const pg = require('pg');
+const connectionString = process.ENV.DATABASE_URL || 'postgres://localhost:5432/task-er';
+
+const client = new pg.Client(connectionString);
+client.connect();
+
+const query = client.query('CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
+query.on('end', () => { client.end(); });)

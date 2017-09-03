58b454e00ae1c24b68ce0ba6d1be449125fc10ee
kamilsz <k.szalek@inwebit.pl>
2017-09-03 23:00:21 +0200

    Adding base route for authentication

---
+++ backend/config/database.js
@@ -0,0 +1,4 @@
+// config for database
+module.exports = {
+  'urlString': 'postgres://docker:docker@pgdb:5432/task-er',
+}
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('users', {message: 'not logged in'});
+});
+
+router.post('/signup', function(req, res, next) {
+  res.render('users', {message: 'Authenticated'});
 });
 
 module.exports = router;
